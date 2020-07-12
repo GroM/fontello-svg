43ab8210eb8cb6dcaf2dd986f0126d392b3d7896
Jerzy Brzozowski <jerzy-brzozowski@wp.pl>
2020-07-12 23:46:41 +0200

    New features: - no fill - support of custom icons - support of custom names

--- bin/fontello-svg.js
+++ bin/fontello-svg.js
@@ -8,30 +8,34 @@ var colors = require('colors');
 var fs = require('fs');
 
 function l(msg, indent, nlBefore, nlAfter) {
-  if (!indent) indent = 0;
-  if (Array.isArray(msg)) msg = msg.join('\n');
-  while (indent--) msg = ' ' + msg;
-  if (nlBefore) msg = '\n' + msg;
-  if (nlAfter) msg = msg + '\n';
-  process.stdout.write(msg + '\n');
+	if (!indent) indent = 0;
+	if (Array.isArray(msg)) msg = msg.join('\n');
+	while (indent--) msg = ' ' + msg;
+	if (nlBefore) msg = '\n' + msg;
+	if (nlAfter) msg = msg + '\n';
+	process.stdout.write(msg + '\n');
 }
 
 // Get an object from a string like this: "key : value | key: value"
 function argsObject(val) {
-  return _.object(val.split('|').map(function(pair) {
-    return pair.split(':').map(function(v) {
-      return v.trim();
-    });
-  }));
+	if(val === undefined)
+	{
+		return undefined;
+	}
+	return _.object(val.split('|').map(function(pair) {
+		return pair.split(':').map(function(v) {
+			return v.trim();
+		});
+	}));
 }
 
 colors.setTheme({
-  info: 'green',
-  data: 'grey',
-  error: 'red'
+	info: 'green',
+	data: 'grey',
+	error: 'red'
 });
 
-app.version('0.0.1');
+app.version('0.1.0');
 app.usage('--config <config file> --out <dir> [options]');
 app.option('-c, --config <config file>', 'Set the Fontello configuration file (required)');
 app.option('-o, --out <dir>', 'Set the export directory (required)');
@@ -41,70 +45,92 @@ app.option('--file-format <format>', 'Override the default filename. Values: {0}
 app.option('--no-css', 'Do not create the CSS file');
 app.option('--no-skip', 'Do not skip existing files');
 app.option('--verbose', 'Verbose output');
+app.option('--classic', 'SVG per glyph');
+app.option('--no-viewbox', 'Do not add viewbox');
+app.option('--no-fill-colors', 'Do not add fill');
 app.parse(process.argv);
 
 // Required parameters
 if (!app.config || !app.out) {
-  l([
-    '',
-    '  Error: missing required parameters (--config, --out)'.error,
-    ''
-  ]);
-  app.help();
+	l([
+		'',
+		'  Error: missing required parameters (--config, --out)'.error,
+		''
+	]);
+	app.help();
 }
-
+// console.log(app);
 // Start
 var config = require(path.resolve(app.config));
 var out = path.resolve(app.out);
-var colors = app.fillColors || {'black': '#000000'};
+var colors = app.fillColors;
+if(colors === undefined)
+{
+	colors = {'black': '#000000'};
+}
+if(colors === false)
+{
+	colors = {'': ''};
+}
 var backgroundUrlPath = app.cssPath || '';
 var fileFormat = app.fileFormat || "{0}-{1}-{2}.svg";
 
-start(config.glyphs, out, colors, app);
 
-function relativePath(abspath) {
-  return path.relative(process.cwd(), abspath);
+// console.log(colors);
+start(config, out, colors, app);
+
+function relativePath(abspath)
+{
+	return path.relative(process.cwd(), abspath);
 }
 
-function start(rawGlyphs, out, colors, app) {
-  var glyphs = fontelloSvg.allGlyphs(rawGlyphs, colors, fileFormat);
+function start(config, out, colors, app)
+{
+	var glyphs = fontelloSvg.allGlyphs(config, colors, fileFormat);
 
-  if (!fs.existsSync(out)){
-    fs.mkdirSync(out);
-  }
+	if (!fs.existsSync(out))
+	{
+		fs.mkdirSync(out);
+	}
 
-  if (app.skip) {
-    fontelloSvg.missingGlyphs(glyphs, out, processGlyphs);
-  } else {
-    processGlyphs(glyphs);
-  }
+	if (app.skip)
+	{
+		fontelloSvg.missingGlyphs(glyphs, out, processGlyphs);
+	}
+	else
+	{
+		processGlyphs(glyphs);
+	}
 
-  function processGlyphs(glyphsToDl) {
-    var glyphsSkipped = glyphs.filter(function(glyph) {
-      return glyphsToDl.indexOf(glyph) === -1;
-    });
-    var downloader = fontelloSvg.downloadSvgs(glyphsToDl, out);
+	function processGlyphs(glyphsToDl)
+	{
+		var glyphsSkipped = glyphs.filter(function(glyph) {
+			return glyphsToDl.indexOf(glyph) === -1;
+		});
+		var downloader = fontelloSvg.downloadSvgs(glyphsToDl, out, app.classic);
 
-    // Output skipped glyphs
-    if (app.skip && app.verbose) {
-      glyphsSkipped.forEach(function(glyph) {
-        l('[skipped]'.data + ' existing SVG: ' + glyph.name + '-' + glyph.collection, 2);
-      });
-    }
+		// Output skipped glyphs
+		if (app.skip && app.verbose)
+		{
+			glyphsSkipped.forEach(function(glyph) {
+				l('[skipped]'.data + ' existing SVG: ' + glyph.name + '-' + glyph.collection, 2);
+			});
+		}
 
-    // SVG write messages
-    downloader.on('fetch-error', function(httpStream) {
-      l('[error]'.error + ' download failed: ' + httpStream.href, 2);
-    });
-    downloader.on('svg-write', function(filename) {
-      l('[saved]'.info + ' ' + relativePath(filename), 2);
-    });
+		// SVG write messages
+		downloader.on('fetch-error', function(httpStream) {
+			l('[error]'.error + ' download failed: ' + httpStream.href, 2);
+		});
+		downloader.on('svg-write', function(filename) {
+			l('[saved]'.info + ' ' + relativePath(filename), 2);
+		});
 
-    // Write CSS
-    if (app.css) {
-      fontelloSvg.writeCss(glyphs, out + '/index.css', backgroundUrlPath, function() {
-        l('[saved]'.info + ' ' + relativePath(out + '/index.css'), 2);
-      });
-    }
-  }
+		// Write CSS
+		if (app.css)
+		{
+			fontelloSvg.writeCss(glyphs, out + '/index.css', backgroundUrlPath, function() {
+				l('[saved]'.info + ' ' + relativePath(out + '/index.css'), 2);
+			});
+		}
+	}
 }
--- index.js
+++ index.js
@@ -1,105 +1,150 @@
-var EventEmitter = require('events').EventEmitter;
-var fs = require('fs');
-var util = require('util');
-var stream = require('stream');
-var _ = require('underscore');
-var request = require('request');
-var mkdirp = require('mkdirp');
-var nodupes = require('nodupes');
-var async = require('async');
+const EventEmitter = require('events').EventEmitter;
+const fs = require('fs');
+const util = require('util');
+const stream = require('stream');
+const _ = require('underscore');
+// var request = require('request');
+const mkdirp = require('mkdirp');
+const nodupes = require('nodupes');
+const async = require('async');
+const fetch = require('node-fetch');
+
 
 // Fontello URL special cases
 var COLLECTION_FILTERS = [
-  [/^fontawesome$/, 'awesome-uni.font'],
-  [/^entypo$/, 'entypo'],
-  [/^iconic$/, 'iconic-uni.font'],
-  [/^websymbols$/, 'websymbols-uni.font'],
-  [/.*/, function(collection) { return collection + '.font' }]
+	[/^fontawesome$/, 'awesome-uni.font'],
+	[/^entypo$/, 'entypo'],
+	[/^iconic$/, 'iconic-uni.font'],
+	[/^websymbols$/, 'websymbols-uni.font'],
+	[/.*/, function(collection) { return collection + '.font' }]
 ];
+
+var fontData = require('./data/server_config.js').uids;
+
 
 // Returns the URL of a Fontello SVG
 function svgUrl(name, collection) {
-  for (var i = 0, result; i < COLLECTION_FILTERS.length; i++) {
-    if (COLLECTION_FILTERS[i][0].test(collection)) {
-      result = COLLECTION_FILTERS[i][1];
-      collection = _.isFunction(result)? result(collection) : result;
-      break;
-    }
-  }
-  return 'https://raw.github.com/fontello/' + collection +
-         '/master/src/svg/' + name + '.svg';
+	for (var i = 0, result; i < COLLECTION_FILTERS.length; i++) {
+		if (COLLECTION_FILTERS[i][0].test(collection)) {
+			result = COLLECTION_FILTERS[i][1];
+			collection = _.isFunction(result)? result(collection) : result;
+			break;
+		}
+	}
+	return 'https://raw.github.com/fontello/' + collection +
+				 '/master/src/svg/' + name + '.svg';
 }
 
 if (!String.format) {
-  String.format = function(format) {
-    var args = Array.prototype.slice.call(arguments, 1);
-    return String(format.replace(/{(\d+)}/g, function(match, number) { 
-      return typeof args[number] != 'undefined'
-        ? args[number] 
-        : match
-      ;
-    }));
-  };
+	String.format = function(format) {
+		var args = Array.prototype.slice.call(arguments, 1);
+		return String(format.replace(/{(\d+)}/g, function(match, number) {
+			return typeof args[number] != 'undefined'
+				? args[number]
+				: match
+			;
+		}));
+	};
 }
 
 // The Glyph object
 var Glyph = {
-  filename: function(color) {
-    color = this.validColor(color);
-    return String.format(this.fileFormat, this.collection, this.name, color);
-  },
-  filenames: function() {
-    return Object.keys(this.colors).map(function(color) {
-      return this.filename(color);
-    }.bind(this));
-  },
-  cssName: function(color, prefix) {
-    if (prefix === undefined) prefix = 'icon-';
-    color = this.validColor(color);
-    return '.' + prefix + this.id + '-' + color;
-  },
-  // Returns CSS declaration(s) corresponding to the glyphs colors
-  cssDeclarations: function(urlPath) {
-    var declarations = '';
-    if (!urlPath) urlPath = '';
-    for (var color in this.colors) {
-      declarations += this.cssName(color) + ' { background-image: url(' +
-                      urlPath + this.filename(color) + ') }\n';
-    }
-    return declarations;
-  },
-  validColor: function(color) {
-    return this.colors[color] ? color : Object.keys(this.colors)[0];
-  }
+	filename: function(color) {
+		color = this.validColor(color);
+		return String.format(this.fileFormat, this.collection, this.name, color);
+	},
+	filenames: function() {
+		return Object.keys(this.colors).map(function(color) {
+			return this.filename(color);
+		}.bind(this));
+	},
+	cssName: function(color, prefix) {
+		if (prefix === undefined) prefix = 'icon-';
+		color = this.validColor(color);
+		return '.' + prefix + this.id + '-' + color;
+	},
+	// Returns CSS declaration(s) corresponding to the glyphs colors
+	cssDeclarations: function(urlPath) {
+		var declarations = '';
+		if (!urlPath) urlPath = '';
+		for (var color in this.colors) {
+			declarations += this.cssName(color, this.prefix) + ' { background-image: url(' +
+											urlPath + this.filename(color) + ') }\n';
+		}
+		return declarations;
+	},
+	validColor: function(color) {
+		return this.colors[color] ? color : Object.keys(this.colors)[0];
+	}
 };
 
 // Creates and returns a Glyph instance
 function createGlyph(name, collection, id, colors, fileFormat) {
-  var glyph = Object.create(Glyph);
-  if (!colors) colors = { 'black': 'rgb(0,0,0)' };
-  if (!fileFormat) fileFormat = "{0}-{1}-{2}.svg";
-  if (!id) id = name;
-  glyph.id = id;
-  glyph.name = name;
-  glyph.collection = collection;
-  glyph.url = svgUrl(glyph.name, glyph.collection);
-  glyph.colors = colors;
-  glyph.exists = null;
-  glyph.fileFormat = fileFormat;
-  return glyph;
+	var glyph = Object.create(Glyph);
+	if (!colors) colors = { 'black': 'rgb(0,0,0)' };
+	if (!fileFormat) fileFormat = "{0}-{1}-{2}.svg";
+	if (!id) id = name;
+	glyph.id = id;
+	glyph.name = name;
+	glyph.collection = collection;
+	glyph.url = svgUrl(glyph.name, glyph.collection);
+	glyph.colors = colors;
+	glyph.exists = null;
+	glyph.fileFormat = fileFormat;
+	return glyph;
+}
+
+function createGlyphFromRaw(rawGlyph, id, colors, fileFormat)
+{
+	if(rawGlyph.src != 'custom_icons' || rawGlyph.selected)
+	{
+		var glyph = Object.create(Glyph);
+		if (!fileFormat) fileFormat = "{0}-{1}-{2}.svg";
+		if (!id) id = rawGlyph.css;
+		glyph.id = id;
+		glyph.name = rawGlyph.css;
+		glyph.collection = rawGlyph.src;
+		glyph.colors = colors;
+		glyph.uid = rawGlyph.uid;
+		glyph.fileFormat = fileFormat;
+		if(rawGlyph.src == 'custom_icons')
+		{
+			glyph.width = rawGlyph.svg.width;
+			glyph.content = rawGlyph.svg.path;
+		}
+		else
+		{
+			glyph.url = svgUrl(glyph.name, glyph.collection);
+			// console.log(fontData);
+			if(fontData[glyph.uid])
+			{
+				glyph.width = fontData[glyph.uid].svg.width;
+				glyph.content = fontData[glyph.uid].svg.d;
+			}
+			else
+			{
+				console.warn('No info for', glyph.collection, ':', glyph.name, '[', glyph.uid, ']');
+			}
+		}
+		// console.log('Selected', glyph.id);
+		return [glyph];
+
+	}
+	// console.log('Skipped', rawGlyph.css);
+	return [];
 }
 
 // Returns a function to create glyphs and incrementing their IDs as needed.
 function glyphCreator() {
-  var unique = nodupes();
-  return function(name, collection, colors) {
-    return createGlyph(name, collection, unique(name), colors);
-  };
+	var unique = nodupes();
+	return function(name, collection, colors) {
+		return createGlyph(name, collection, unique(name), colors);
+	};
 }
 
 // Converts a raw glyph (right from the Fontello JSON) to a Glyph instance.
 function rawGlyphToGlyph(rawGlyph, id, colors) {
-  return createGlyph(rawGlyph.css, rawGlyph.src, id, colors);
+	return createGlyph(rawGlyph.css, rawGlyph.src, id, colors);
 }
 
 // Sometimes, a glyph name have a numbered suffix.  This suffix can be the name
@@ -107,72 +152,113 @@ function rawGlyphToGlyph(rawGlyph, id, colors) {
 // another pictogram (in another collection) have the same name (e.g. "search-2").
 // In the second case, the suffix need to be removed.
 function fixNames(rawGlyphs) {
-  var countSuffix = /\-[1-9][0-9]*$/;
-  return rawGlyphs.map(function(rawGlyph) {
-    var name = rawGlyph.css;
-    if (!countSuffix.test(name)) return rawGlyph;
-    var noSuffix = name.replace(countSuffix, '');
-    var exists = rawGlyphs.some(function(rawGlyph) {
-      return rawGlyph.css === noSuffix;
-    });
-    if (exists) rawGlyph.css = noSuffix;
-    return rawGlyph;
-  });
+	var countSuffix = /\-[1-9][0-9]*$/;
+	return rawGlyphs.map(function(rawGlyph) {
+		var name = rawGlyph.css;
+		if (!countSuffix.test(name)) return rawGlyph;
+		var noSuffix = name.replace(countSuffix, '');
+		var exists = rawGlyphs.some(function(rawGlyph) {
+			return rawGlyph.css === noSuffix;
+		});
+		if (exists) rawGlyph.css = noSuffix;
+		return rawGlyph;
+	});
 }
 
 // Creates and returns all Glyphs from a rawGlyphs list
-function allGlyphs(rawGlyphs, colors, fileFormat) {
-  var unique = nodupes();
-  rawGlyphs = fixNames(rawGlyphs);
-  return rawGlyphs.map(function(rawGlyph) {
-    var name = rawGlyph.css;
-    var collection = rawGlyph.src;
-    return createGlyph(name, collection, unique(name), colors, fileFormat);
-  });
+function allGlyphs(config, colors, fileFormat) {
+	var unique = nodupes();
+	var rawGlyphs = fixNames(config.glyphs);
+	const cssPrefix = config.css_prefix_text;
+	// console.log(rawGlyphs);
+	if(rawGlyphs.flatMap)
+	{
+		return rawGlyphs.flatMap(rawGlyph => createGlyphFromRaw(rawGlyph, unique(rawGlyph.css), colors, fileFormat, cssPrefix));
+	}
+	return rawGlyphs.reduce((acc, rawGlyph) => acc.concat(createGlyphFromRaw(rawGlyph, unique(rawGlyph.css), colors, fileFormat, cssPrefix)), []);
 }
 
 // Filters all glyphs to returns only the ones missing on the FS
 function missingGlyphs(glyphs, svgDir, cb) {
-  async.reject(glyphs, function(glyph, cb) {
-    var filenames = glyph.filenames().map(function(filename) {
-      return svgDir + '/' + filename;
-    });
-    async.every(filenames, fs.exists, cb);
-  }, cb);
+	async.reject(glyphs, function(glyph, cb) {
+		var filenames = glyph.filenames().map(function(filename) {
+			return svgDir + '/' + filename;
+		});
+		async.every(filenames, fs.exists, cb);
+	}, cb);
 }
 
-function svgTransform(body, fillColor) {
-  if (!fillColor) return body;
-  return body.replace(/<path/g, '<path fill="' + fillColor + '"');
+function svgTransform(glyph, fillColor) {
+	// if (!fillColor) return body;
+	// console.log(glyph);
+	var res = ['<svg height="1000" width="', glyph.width, '" viewBox="0 0 ', glyph.width, ' 1000" xmlns="http://www.w3.org/2000/svg"><path'];
+	if(fillColor)
+	{
+		res.push(' fill="', fillColor, '"');
+	}
+	res.push(' d="', glyph.content, '"/></svg>');
+	return res.join('');
 }
 
-function downloadSvgs(glyphs, svgDir) {
-  var downloader = Object.create(EventEmitter.prototype);
-  EventEmitter.call(downloader);
-  glyphs.forEach(function(glyph) {
-    var url = svgUrl(glyph.name, glyph.collection);
-    var svgFetcher = request(url, function(err, response, content) {
-      if (err) return downloader.emit('fetch-error', svgFetcher);
-      _.each(glyph.colors, function(fillColor, colorName) {
-        var svgContent = svgTransform(content, fillColor);
-        var filename = svgDir + '/' + glyph.filename(colorName);
-        fs.writeFile(filename, svgContent, function(err) {
-          if (err) throw err;
-          downloader.emit('svg-write', filename);
-        });
-      });
-    });
-  });
-  return downloader;
+function downloadSvgs(glyphs, svgDir, classic) {
+	var writeFunction = function(glyph){
+		_.each(glyph.colors, function(fillColor, colorName) {
+					var svgContent = svgTransform(glyph, fillColor);
+					var filename = svgDir + '/' + glyph.filename(colorName);
+					fs.writeFile(filename, svgContent, function(err) {
+						if(err) throw err;
+						downloader.emit('svg-write', filename);
+					});
+				});
+	};
+	var classicFunction = function(glyph) {
+		var url = svgUrl(glyph.name, glyph.collection);
+		var svgFetcher = fetch(url).then(res => {
+			if(res.status == 200)
+			{
+
+			}
+			else
+			{
+				return downloader.emit('fetch-error', svgFetcher);
+			}
+		});
+		/*
+		var svgFetcher = request(url, function(err, response, content) {
+			if (err)
+			{
+				return downloader.emit('fetch-error', svgFetcher);
+			}
+			_.each(glyph.colors, function(fillColor, colorName) {
+				var svgContent = svgTransform(content, fillColor);
+				var filename = svgDir + '/' + glyph.filename(colorName);
+				fs.writeFile(filename, svgContent, function(err) {
+					if (err) throw err;
+					downloader.emit('svg-write', filename);
+				});
+			});
+		});
+		*/
+	};
+	var process = writeFunction;
+	if(classic)
+	{
+		process = classicFunction;
+	}
+
+	var downloader = Object.create(EventEmitter.prototype);
+	EventEmitter.call(downloader);
+	glyphs.forEach(process);
+	return downloader;
 }
 
 function writeCss(glyphs, cssPath, backgroundUrlPath, cb) {
-  var fileWriter = fs.createWriteStream(cssPath);
-  glyphs.forEach(function(glyph) {
-    fileWriter.write(glyph.cssDeclarations(backgroundUrlPath));
-  });
-  fileWriter.end();
-  cb();
+	var fileWriter = fs.createWriteStream(cssPath);
+	glyphs.forEach(function(glyph) {
+		fileWriter.write(glyph.cssDeclarations(backgroundUrlPath));
+	});
+	fileWriter.end();
+	cb();
 }
 
 exports.svgUrl = svgUrl;
